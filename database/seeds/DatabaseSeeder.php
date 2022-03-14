3dd19c839c887e967daf7637caf461e357ae23da
Andrey Vasyukov <a.vasyukov97@gmail.com>
2022-03-14 20:38:06 +0200

    Add UserSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(SizeSeeder::class);
+        $this->call(UserSeeder::class);
     }
 }
---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'firstname' => 'Admin',
+            'lastname' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => \Illuminate\Support\Facades\Hash::make('morkovka123')
+        ]);
+    }
+}
